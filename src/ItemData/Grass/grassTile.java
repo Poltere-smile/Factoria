a4c6bc0ef36f6e70dbe1288a7c96a03e8c9dd331
Poltere-smile <railtontc498@gmail.com>
2026-09-11 06:32:58 -0400

    Add files via upload

---
+++ src/ItemData/Grass/grassTile.java
@@ -0,0 +1,11 @@
+package ItemData.Grass;
+
+import ItemData.Pipe.forwardPipe;
+
+import javax.swing.*;
+
+public class grassTile {
+    public static int itemdataNumber = 0;
+    public static ImageIcon itemImage = new ImageIcon(forwardPipe.class.getResource("baseGrassTile.png"));
+    public static boolean canHoldItems = false;
+}
