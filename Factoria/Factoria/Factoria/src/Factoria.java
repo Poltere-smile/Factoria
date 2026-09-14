255ca1f221f4cad1ac56c09bb3bc6332d8ace320
Poltere-smile <railtontc498@gmail.com>
2026-09-14 05:57:21 -0400

    Revert "Update3"

--- Factoria/Factoria/Factoria/src/Factoria.java
+++
@@ -1,8 +0,0 @@
-public class Factoria {
-    public static void main(String[] args){
-
-
-        new mainGame();
-    }
-
-}
--- Factoria/Factoria/Factoria/src/ItemData/Pipe/Pipe.java
+++
@@ -1,9 +0,0 @@
-package ItemData.Pipe;
-
-import javax.swing.*;
-
-public class Pipe {
-    public static int itemdataNumber = 1;
-    public static ImageIcon itemImage = new ImageIcon(Pipe.class.getResource(""));
-
-}
